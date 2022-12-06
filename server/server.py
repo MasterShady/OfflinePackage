ce90a03c34204d387d90c2e0fd69e17fb8ba93ad
liusiyuan <379587878@qq.com>
2022-12-06 19:08:25 +0800

    update

--- server/server.py
+++ server/server.py
@@ -7,7 +7,7 @@ from flask import render_template
 from datetime import timedelta
 from flask import url_for, redirect
 
-app = Flask(__name__, static_url_path='/static',static_folder='templates/static')
+app = Flask(__name__, static_url_path='/static', static_folder='templates/static')
 CORS(app)
 
 @app.route("/redirct")
@@ -44,7 +44,34 @@ def test_post():
     # print(f"post params {req}")
     return rsp
 
+@app.errorhandler(Exception)
+def handle_exception(err):
+    print(err)
+
+
+# #设置允许的文件格式
+# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
+#
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+# 设置静态文件缓存过期时间
+# app.send_file_max_age_default = timedelta(seconds=1)
+
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     f = request.files['file']
+#     if not (f and allowed_file(f.filename)):
+#         return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
+#     user_input = request.form.get("name")
+#     basepath = os.path.dirname(__file__)  # 当前文件所在路径
+#     upload_path = os.path.join(basepath, 'static/images', secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+#     f.save(upload_path)
+#     rsp_json = {"message": "upload image!"}
+#     rsp = flask.make_response(jsonify(rsp_json), 200)
+#     return rsp
 
 if __name__ == '__main__':
     tmr = datetime.datetime.now() + timedelta(days=1)
-    app.run(host='0.0.0.0', port=8000, use_reloader=False, threaded=True, debug=False)
+    app.run(host='0.0.0.0', port=8000, use_reloader=False, threaded=True, debug=True)
--- server/templates/static/app.js
+++ server/templates/static/app.js
@@ -50,13 +50,6 @@ function setCookie() {
 	document.cookie = 'cookie_from_js=1; ' + 'expires=' + d.toGMTString() + '; ';
 }
 
-// function setCookie1() {
-// 	var d = new Date();
-// 	d.setTime(d.getTime() + 7 * 24 * 60 * 60 * 1000);
-// 	let string = 'cookie2=哈喽;' + 'expires=' + d.toGMTString() + '; ';
-// 	console.log(string)
-// 	document.cookie = string
-// }
 
 function getAllCookie() {
 	var cookies = document.cookie.split(';');
@@ -65,17 +58,61 @@ function getAllCookie() {
 	}
 }
 
+function dataURItoBlob(base64Data) {
+
+	//console.log(base64Data);//data:image/png;base64,
+	//let byteString = 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
+	let byteCharacters = atob(base64Data);
+	// if(base64Data.split(',')[0].indexOf('base64') >= 0)
+	// 	byteString = atob(base64Data.split(',')[1]);//base64 解码
+	// else{
+	// 	byteString = unescape(base64Data.split(',')[1]);
+	// }
+	// var mimeString =
+		//base64Data.split(',')[0].split(':')[1].split(';')[0];//mime类型 -- image/png
+
+	// var arrayBuffer = new ArrayBuffer(byteString.length); //创建缓冲数组
+	// var ia = new Uint8Array(arrayBuffer);//创建视图
+	var ia = new Uint8Array(byteCharacters.length);//创建视图
+	for(var i = 0; i < byteCharacters.length; i++) {
+		ia[i] = byteCharacters.charCodeAt(i);
+	}
+	var blob = new Blob([ia], {
+		type: 'image/png'
+	});
+	return blob;
+}
+
+function getImage(){
+	if (window.KKJSBridge != null){
+		window.KKJSBridge.call('EvnModule','getImage', {}, function(res){
+			let base64data = res.imageData;
+			let blobData = dataURItoBlob(base64data);
+			let urlCreator = window.URL || window.webkitURL;
+			let imageUrl = urlCreator.createObjectURL(blobData);
+			$('#image').attr('src',imageUrl);
+		})
+	}else {
+		let blobData = dataURItoBlob("");
+		let urlCreator = window.URL || window.webkitURL;
+		let imageUrl = urlCreator.createObjectURL(blobData);
+		$('#image').attr('src',imageUrl);
+	}
+
+}
+
+
 $(document).ready(function() {
 	if (window.KKJSBridge != null){
 		window.KKJSBridge.call('EvnModule', 'getRequestHost', {}, function(res) {
 			host = "http://" + res.host + "/"
 			testAjax();
-			setCookie();
+			// setCookie();
 			$("#request").html('ajax请求地址: ' + host)
 		});
 	}else {
 		testAjax();
-		setCookie();
+		// setCookie();
 		$("#request").html('ajax请求地址: ' + host)
 	}
 });
